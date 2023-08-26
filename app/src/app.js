b9d7bb13b0ea234a692a64f58e5749cbaa48f15a
Mike Cronin <mostlyfocusedmike@gmail.com>
2023-08-26 17:38:44 -0400

    [ART-3] Make hard code solo fetch (log to console) (#6)
    * [ART-3] Add fetch function
    
    * [ART-3] Add test logs

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,6 @@
 import {
-  fetchAllArtByKeyword
+  fetchAllArtByKeyword,
+  fetchArtworkById
 } from "./fetch-funcs";
 
 export default async function app(mainEl) {
@@ -11,4 +12,8 @@ export default async function app(mainEl) {
   const artworks = await fetchAllArtByKeyword()
   console.log('Fetch #1, list')
   console.table(artworks)
+
+  const artwork = await fetchArtworkById(artworks[0].id)
+  console.log('Fetch #2, single')
+  console.log(artwork)
 }
