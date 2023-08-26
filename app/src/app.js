7081519f20089621a866a19cb240e546712f1d31
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2023-08-26 16:30:30 -0400

    [ART-2] Add quick console test

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,11 @@
+import { fetchHandler } from "./fetch-funcs";
+
 export default function app(mainEl) {
   const mainTitle = document.createElement('h1')
   mainTitle.textContent = 'Art Viewer';
   mainEl.append(mainTitle);
+
+  // TODO: remove this code when we fetch for real
+  fetchHandler('https://api.artic.edu/api/v1/artworks/129884').then(console.log);
+  fetchHandler('https://api.artic.edu/api/v1/artworks/bad-url').then(console.log);
 }
